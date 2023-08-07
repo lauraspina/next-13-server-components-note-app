232bcfe46028c22e9ec3833f3b8877d6c6c5e9e3
Laura Spina <lauraspina@Lauras-MBP.localdomain>
2023-08-07 18:57:27 +0200

    feat: server components note app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Link from "next/link";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +16,21 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+      <html>
+      <body>
+      <main>
+        <nav>
+          <Link
+              href="/">
+            Home
+          </Link>
+          <Link href="/notes">
+            Notes
+          </Link>
+        </nav>
+        {children}
+      </main>
+      </body>
+      </html>
+  );
 }
